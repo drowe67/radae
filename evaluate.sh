73eb9a714831f9d55e1b48546761e964502e7a94
David <david@rowetel.com>
2024-02-22 09:13:40 +1030

    explaining AWGN noise simulation

--- evaluate.sh
+++ evaluate.sh
@@ -20,7 +20,10 @@ if [ ! -f $2 ]; then
     exit 1
 fi
 
-# Approximation of Hilbert clipper type compressor.  Could do with some HF boost
+# Approximation of Hilbert clipper type compressor.  
+# TODO: modify to make PAPR constant, indep of input level. Best run this on single
+# samples rather than files from many speakers with different levels
+
 function analog_compressor {
     input_file=$1
     output_file=$2
@@ -55,7 +58,8 @@ speech_8k=$(mktemp).s16
 speech_comp=$(mktemp).s16
 sox $fullfile -r 8000 -t .s16 -c 1 $speech_8k
 analog_compressor $speech_8k $speech_comp
-# TODO compute this from EbNodB
+# note experimentally derived conversion to "--No" parameter in ch tool
+# TODO: check this is not speech sample specific, we want same SNR (C/No) as radae signal
 ch $speech_comp - --No $((-EbNodB-16)) | sox -t .s16 -r 8000 -c 1 - ${out_dir}/${filename}_${EbNodB}dB_ssb.wav
 
 #TODO ssb spectrogram
