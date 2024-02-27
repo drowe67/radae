caffff176b90121da21c00fb14882e5f334f3789
drowe67 <david@rowetel.com>
2024-02-28 10:08:52 +1030

    wip stand alone rx

---
+++ rx.py
@@ -0,0 +1,78 @@
+"""
+/*
+  Radio Autoencoder receiver: rate Fs complex samples in, features out.
+
+  Copyright (c) 2024 by David Rowe */
+
+/*
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+"""
+
+import os
+import argparse
+import numpy as np
+import torch
+from radae import RADAE
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('model_name', type=str, help='path to model in .pth format')
+parser.add_argument('rx', type=str, help='path to input file of rate Fs rx samples in ..IQIQ...f32 format')
+parser.add_argument('features_hat', type=str, help='path to output feature file in .f32 format')
+parser.add_argument('--latent-dim', type=int, help="number of symbols produces by encoder, default: 80", default=80)
+parser.add_argument('--write_latent', type=str, default="", help='path to output file of latent vectors z[latent_dim] in .f32 format')
+args = parser.parse_args()
+
+device = torch.device("cpu")
+latent_dim = args.latent_dim
+nb_total_features = 36
+num_features = 20
+num_used_features = 20
+
+# load model from a checkpoint file
+model = RADAE(num_features, latent_dim, args.EbNodB, ber_test=args.ber_test, rate_Fs=args.rate_Fs, 
+              phase_offset=args.phase_offset, freq_offset=args.freq_offset)
+checkpoint = torch.load(args.model_name, map_location='cpu')
+model.load_state_dict(checkpoint['state_dict'], strict=False)
+
+# load rx rate Fs samples
+rx = torch.tensor(np.fromfile(args.rx, dtype=np.csingle))
+
+if __name__ == '__main__':
+
+   # push model to device and run test
+   model.to(device)
+   rx = rx.to(device)
+   features_hat = model.receive(rx)
+
+   features_hat = torch.cat([features_hat, torch.zeros_like(features_hat)[:,:,:16]], dim=-1)
+   features_hat = features_hat.cpu().detach().numpy().flatten().astype('float32')
+   features_hat.tofile(args.features_hat)
+
+   # write real valued latent vectors
+   if len(args.write_latent):
+      z_hat = output["z_hat"].cpu().detach().numpy().flatten().astype('float32')
+      z_hat.tofile(args.write_latent)
+  
