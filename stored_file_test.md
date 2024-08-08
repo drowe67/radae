97c168a78a32f19cf56432105626df2e39d874e1
David <david@rowetel.com>
2024-08-08 17:07:19 +0930

    wip est_CNo.py can find chirp location

--- stored_file_test.md
+++ stored_file_test.md
@@ -3,7 +3,7 @@
 Preparing for Tx:
 1. Record a wave file of your own voice, see example.  We suggest about 10s but its up to you how long.
 1. Try to make the peak level between about half way and the clipping level.
-1. Use `ota_test.sh`` to create a `tx.wav file``, this consist of chirp-compressed SSB-radae.
+1. Use `ota_test.sh` to create a `tx.wav file`, this consist of chirp-compressed SSB-radae.
 1. Take a look at it on your favourite waveform editor, you can see the signals are adjusted to have the same peak level.
 
 Here is how I test using KiWSDRs:
@@ -14,7 +14,7 @@ Here is how I test using KiWSDRs:
 1. KiwSDR file will be downloaded
 1. Load it into your waveform viewer.  I find spectroigram mode useful.  Clip the silence before the chirp, and a little bit of the chirp.  We want the chirp to be the first signal the receiver sees.
 1. Add a serial number to the start, and make nyou rown notes of the conditions (e.g. rx station locatioon distance, power level, anything else you think is relevant)
-1. Process `ota-rx.sh``
+1. Process `ota-rx.sh`
 1. listen to the results
 1. Try to collect som intersting results, for example different channels nd power levels
 
--- test/chirp_mpp.sh
+++ test/chirp_mpp.sh
@@ -13,23 +13,56 @@ fi
 
 CODEC2_DEV_BUILD_DIR=$1
 No=$2
+chirp_duration=4
+silence_duration=3
 
 source test/make_g.sh
 cp -f g_mpp.f32 fast_fading_samples.float
 chirp_f32=$(mktemp)
 chirp_int16=$(mktemp)
+silence_int16=$(mktemp)
+chirp_pad_int16=$(mktemp)
 chirp_noise_int16=$(mktemp)
 chirp_noise_f32=$(mktemp)
-python3 chirp.py ${chirp_f32} 4
-cat ${chirp_f32} | python3 f32toint16.py --real > ${chirp_int16}
 ch_log=$(mktemp)
+python3 chirp.py ${chirp_f32} ${chirp_duration}
+cat ${chirp_f32} | python3 f32toint16.py --real > ${chirp_int16}
+# pad either side with a few seconds of seconds of silence
+dd if=/dev/zero of=/dev/stdout bs=16000 count=${silence_duration} > ${silence_int16}
+cat ${silence_int16} ${chirp_int16} ${silence_int16} > ${chirp_pad_int16}
 # Note --ssbfilt 1 (default) removes -ve freq part of complex noise 
 # which ensures C/No remains unaffected by real() operation at output 
-${CODEC2_DEV_BUILD_DIR}/src/ch ${chirp_int16} ${chirp_noise_int16} --No ${No} --mpp --fading_dir . --after_fade 2>${ch_log}
+${CODEC2_DEV_BUILD_DIR}/src/ch ${chirp_pad_int16} ${chirp_noise_int16} --No ${No} --mpp --fading_dir . --after_fade 2>${ch_log}
 cat ${chirp_noise_int16} | python3 int16tof32.py  --zeropad > ${chirp_noise_f32}
 est_log=$(mktemp)
 python3 est_CNo.py ${chirp_noise_f32} >${est_log}
 
+# check two estimates are about the same
+
 CNodB_ch=$(cat ${ch_log} | grep "C/No" | tr -s ' ' | cut -d' ' -f5)
-CNodB_est=$(cat ${est_log} | grep "Measured:" | tr -s ' ' | cut -d' ' -f2)
-python3 -c "if abs(${CNodB_ch}-${CNodB_est}) < 1.0: print('PASS')"
+CNodB_est=$(cat ${est_log} | grep "Measured:" | tr -s ' ' | cut -d' ' -f3)
+python3 <<EOF
+import sys
+import numpy as np
+# ch C est will read low, as C is averaged across entire sample including silence
+CNodB_ch = ${CNodB_ch} + 10*np.log10((2*${silence_duration}+${chirp_duration})/${chirp_duration})
+print(f"{CNodB_ch:5.2f}")
+if abs(CNodB_ch-${CNodB_est}) > 1.0:
+  sys.exit(1)
+EOF
+if [ $? -eq 1 ]; then
+  exit 1
+fi
+
+# check time estimate is OK
+
+chirp_start=$(cat ${est_log} | grep "Measured:" | tr -s ' ' | cut -d' ' -f2)
+python3 <<EOF
+import sys
+if abs(${chirp_start} - ${silence_duration}) > 0.5:
+  sys.exit(1)
+EOF
+if [ $? -eq 1 ]; then
+  exit 1
+fi
+
