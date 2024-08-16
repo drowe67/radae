0adab4c675378661b5baf635bae5bc467dbbad1e
David <david@rowetel.com>
2024-08-16 17:38:13 +0930

    add radae_rx log to report file

--- ota_test.sh
+++ ota_test.sh
@@ -140,7 +140,8 @@ function process_rx {
     python3 est_CNo.py ${rx_chirp}.f32 | tee $est_log
     chirp_start=$(cat ${est_log} | grep "Measured:" | tr -s ' ' | cut -d' ' -f2)
     cat ${est_log} | tail -n 2 > ${filename}_report.txt
-    
+    echo >> ${filename}_report.txt
+
     # remove silence before chirp
     rx_trim=$(mktemp).wav
     sox $rx $rx_trim trim $chirp_start
@@ -158,7 +159,7 @@ function process_rx {
 
     # Use streaming RADAE Rx
     cat ${rx_radae}.raw | python3 int16tof32.py --zeropad > ${rx_radae}.f32
-    cat ${rx_radae}.f32 | python3 radae_rx.py model19_check3/checkpoints/checkpoint_epoch_100.pth -v 2 --auxdata > features_rx_out.f32
+    cat ${rx_radae}.f32 | python3 radae_rx.py model19_check3/checkpoints/checkpoint_epoch_100.pth -v 2 --auxdata 2>>${filename}_report.txt > features_rx_out.f32
     lpcnet_demo -fargan-synthesis features_rx_out.f32 - | sox -t .s16 -r 16000 -c 1 - ${filename}_radae.wav
 }
 
