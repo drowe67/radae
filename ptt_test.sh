4b8b53759c4ba3ed5d0871a7fabb1e45dbfb39ea
David <david@rowetel.com>
2024-08-22 08:28:06 +0930

    ability to Tx SSB and RADAE

--- ptt_test.sh
+++ ptt_test.sh
@@ -7,10 +7,21 @@ function clean_up {
     exit 1
 }
 
-function kick_off_tx_process {
-    arecord --device "plughw:CARD=${mic_card},DEV=0" -f S16_LE -c 1 -r 16000 | \
+function kick_off_ssb_tx_process {
+    ( arecord --device "plughw:CARD=${mic_card},DEV=0" -f S16_LE -c 1 -r 8000 & echo $! >tx_pid ) | \
+    aplay -f S16_LE --device "plughw:CARD=${rig_card},DEV=0" &
+
+    echo
+    echo "SSB transmitter running!  Any key to exit"
+    echo
+    cat tx_pid
+}
+
+function kick_off_radae_tx_process {
+    # note we store PID of process before radae_tx.py, killing should mean end of over pilot sequence is sent by radae_tx.py (TBC)
+    ( arecord --device "plughw:CARD=${mic_card},DEV=0" -f S16_LE -c 1 -r 16000 & echo $! >tx_pid ) | \
     ./build/src/lpcnet_demo -features - - | \
-    ( python3 radae_tx.py model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata & echo $! >tx_pid ) | \
+    python3 radae_tx.py model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | \
     python3 f32toint16.py --real --scale 8192 | aplay -f S16_LE --device "plughw:CARD=${rig_card},DEV=0" &
 
     echo
@@ -37,19 +48,31 @@ printf "\n"
 mic_card=$(aplay -l | grep "card ${mic_card_num}" | head -n 1 | tr -s ' ' | cut -d' ' -f3)
 rig_card=$(aplay -l | grep "card ${rig_card_num}" | head -n 1 | tr -s ' ' | cut -d' ' -f3)
 
-echo $mic_card
-echo $rig_card
+printf "mic_card: %s rig_card:\n" $mic_card $rig_card
 
 # clean up any processes if we get a ctrl-C
 trap clean_up SIGHUP SIGINT SIGTERM
 
 tx=0
+ssb=0
 key=' ' 
 while [[ ! $key == "q" ]]
 do
+    printf "\n****************************************************\n"
+    printf "tx: %d ssb: %d Quit-q Toggle Tx-spacebar Toggle SSB-s\n" $tx $ssb
+    printf "****************************************************\n"
     read -N 1 key
-    echo $key
+    # space char doesn't compare well
     key=${key/ /t}
+
+    if [ $key == "s" ]; then
+        if [ $ssb -eq 1 ]; then
+            ssb=0
+        else
+            ssb=1
+        fi
+    fi
+
     if [ "$key" == "t" ]; then
         if [ $tx -eq 1 ]; then
             tx=0
@@ -59,7 +82,11 @@ do
             echo
         else
             tx=1
-            kick_off_tx_process
+            if [ $ssb -eq 0 ]; then
+                kick_off_radae_tx_process
+            else
+                kick_off_ssb_tx_process
+            fi
         fi
     fi
 done
