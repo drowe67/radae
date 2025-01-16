9a8b1755c682be251e0c401b867a04e9a3968419
David <david@rowetel.com>
2025-01-17 07:39:28 +1030

    3 stage clip-filter gives us a reasonable 99% power BW of < 1300 Hz

--- radae/radae.py
+++ radae/radae.py
@@ -628,7 +628,16 @@ class RADAE(nn.Module):
                     tx = self.txbpf_conv(tx)*torch.conj(phase_vec)
                     tx = torch.reshape(tx,(num_batches, num_timesteps_at_rate_Rs, self.M))
                     tx = torch.exp(1j*torch.angle(tx))
-                     
+                    
+                    tx = torch.reshape(tx,(num_batches, 1, num_timesteps_at_rate_Rs*self.M))
+                    phase_vec = torch.exp(-1j*self.alpha*torch.arange(0,tx.shape[2],device=tx.device))
+                    tx = tx*phase_vec
+                    tx = torch.concat((torch.zeros((num_batches,1,self.txbpf_delay),device=tx.device),tx,torch.zeros((num_batches,1,self.txbpf_delay),device=tx.device)),dim=2)
+                    tx = self.txbpf_conv(tx)*torch.conj(phase_vec)
+                    tx = torch.reshape(tx,(num_batches, num_timesteps_at_rate_Rs, self.M))
+                    tx = torch.exp(1j*torch.angle(tx))
+    
+
                 tx_before_channel = tx
                 # DFT to transform M time domain samples to Nc carriers
                 tx_sym = torch.matmul(tx, self.Wfwd)
