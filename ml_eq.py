39bb89daf5b5a915c512fa4153874e04cc570c75
David <david@rowetel.com>
2025-01-27 08:27:38 +1030

    moved channel model into forward(), inference section plots scatter

--- ml_eq.py
+++ ml_eq.py
@@ -63,9 +63,9 @@ class EQ(nn.Module):
         self.dense3 = nn.Linear(w1+self.n_data, w1)
         self.dense4 = nn.Linear(w1+self.n_data, w1)
         self.dense5 = nn.Linear(w1+self.n_data, self.n_data)
-        self.dense = nn.Linear(w1+self.n_data, self.n_data)
 
-    def forward(self, pilots, data):
+    # note complex values passed in as real,imag pairs
+    def equaliser(self, pilots,data):
         x = torch.relu(self.dense1(torch.cat([pilots, data],-1)))
         x = torch.relu(self.dense2(torch.cat([x, data],-1)))
         x = torch.relu(self.dense3(torch.cat([x, data],-1)))
@@ -73,8 +73,46 @@ class EQ(nn.Module):
         equalised_data = self.dense5(torch.cat([x, data],-1))
         return equalised_data
 
+    # tx_data is complex, return real values real,imag pairs to suit loss function
+    def forward(self, tx_data):
+        batch_size = tx_data.shape[0]
+
+        # create frame
+        pilot = torch.zeros((batch_size,1),dtype=torch.complex64, device=tx_data.device)
+        pilot[:,0] = 1 + 1j*0
+        tx_frame = torch.cat([pilot, tx_data, pilot],-1)
+        
+        # channel simulation, apply same phase offset to all symbols in frame
+        phi = torch.zeros(batch_size, tx_frame.shape[1], device=tx_data.device)
+        phi[:,] = 2*torch.pi*torch.rand(batch_size,1)
+        rx_frame = tx_frame*torch.exp(1j*phi)
+
+        # de-frame pilots and data, separate real and imag
+        # TODO: more efficient way to do this
+        rx_pilots = torch.zeros((batch_size,2*n_pilots), device=tx_data.device)
+        rx_pilots[:,0] = rx_frame[:,0].real
+        rx_pilots[:,1] = rx_frame[:,0].imag
+        rx_pilots[:,2] = rx_frame[:,2].real
+        rx_pilots[:,3] = rx_frame[:,2].imag
+        rx_data = torch.zeros((batch_size,2*n_data), device=tx_data.device)
+        rx_data[:,0] = rx_frame[:,1].real
+        rx_data[:,1] = rx_frame[:,1].imag
+
+        # run equaliser
+        rx_data_eq = self.equaliser(rx_pilots, rx_data)
+
+        # real version of tx_data symbol for loss function
+        tx_data_real = torch.zeros((batch_size,2*n_data), device=tx_data.device)
+        tx_data_real[:,0] = tx_data[:,0].real
+        tx_data_real[:,1] = tx_data[:,0].imag
+        tx_data_real = tx_data_real.to(device)
+
+        return tx_data_real,rx_data, rx_data_eq
+        
 model = EQ(2*n_pilots, 2*n_data).to(device)
 print(model)
+nb_params = sum(p.numel() for p in model.parameters())
+print(f" {nb_params} weights")
 loss_fn = nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
 
@@ -85,39 +123,8 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
 for epoch in range(args.epochs):
     sum_loss = 0.0
     for batch,(tx_data) in enumerate(dataloader):
-        # create frame
-        pilot = torch.zeros((batch_size,1),dtype=torch.complex64)
-        pilot[:,0] = 1 + 1j*0
-        tx_frame = torch.cat([pilot, tx_data, pilot],-1)
-        
-        # channel simulation, apply same phase offset to all symbols in frame
-        phi = torch.zeros(batch_size, tx_frame.shape[1])
-        phi[:,] = 2*torch.pi*torch.rand(batch_size,1)
-        rx_frame = tx_frame*torch.exp(1j*phi)
-
-        # de-frame pilots and data, separate real and imag
-        # TODO: more efficient way to do this
-        rx_pilots = torch.zeros((batch_size,2*n_pilots))
-        rx_pilots[:,0] = rx_frame[:,0].real
-        rx_pilots[:,1] = rx_frame[:,0].imag
-        rx_pilots[:,2] = rx_frame[:,2].real
-        rx_pilots[:,3] = rx_frame[:,2].imag
-        rx_data = torch.zeros((batch_size,2*n_data))
-        rx_data[:,0] = rx_frame[:,1].real
-        rx_data[:,1] = rx_frame[:,1].imag
-
-        # send to model
-        rx_pilots = rx_pilots.to(device)
-        rx_data = rx_data.to(device)
-        rx_data_eq = model(rx_pilots, rx_data)
-
-        # real version of tx_data synmbol for loss function
-        tx_data_real = torch.zeros((batch_size,2*n_data))
-        tx_data_real[:,0] = tx_data[:,0].real
-        tx_data_real[:,1] = tx_data[:,0].imag
-        tx_data_real = tx_data_real.to(device)
-        #print(tx_data.shape, rx_data_eq.shape)
-
+        tx_data = tx_data.to(device)
+        tx_data_real,rx_data,rx_data_eq = model(tx_data)
         loss = loss_fn(tx_data_real, rx_data_eq)
         loss.backward() 
         optimizer.step()
@@ -133,21 +140,19 @@ for epoch in range(args.epochs):
         f'Loss: {sum_loss / (batch + 1):.10f}')
 
 
-"""
 # Inference using trained model
+n_syms_inf = 1000
 model.eval()
-model.sigma=0
-        self.bits = torch.sign(torch.rand(self.n_syms, self.bps)-0.5)
-        self.symbs = (self.bits[:,::2] + 1j*self.bits[:,1::2])/np.sqrt(2.0)
-bits_in = torch.sign(torch.rand(n_syms, np, bps)-0.5)
+bits = torch.sign(torch.rand(n_syms_inf, dataset.bps)-0.5)
+tx_data = (bits[:,::2] + 1j*bits[:,1::2])/np.sqrt(2.0)
 with torch.no_grad():
-    symbols, bits_out = model(bits_in.to(device))
-symbols = symbols.cpu().numpy()
-print(symbols.shape)
-plt.plot(symbols.real,symbols.imag,'+')
-#plt.plot(symbols[:,1].real,symbols[:,1].imag,'+')
-#plt.plot(symbols[:,0].real,symbols[:,0].imag,'+')
-#plt.plot(symbols[:,1].real,symbols[:,1].imag,'+')
+    tx_data = tx_data.to(device)
+    tx_data_real,rx_data,rx_data_eq = model(tx_data)
+rx_data = rx_data.cpu().numpy()
+rx_data_eq = rx_data_eq.cpu().numpy()
+print(rx_data_eq.shape)
+plt.plot(rx_data[:,0],rx_data[:,1],'+')
+plt.plot(rx_data_eq[:,0],rx_data_eq[:,1],'+')
 plt.axis([-2,2,-2,2])
 plt.show()
-"""
+
