20716cf47e1f39c1c672086e71398d241ebefbf4
David <david@rowetel.com>
2025-11-24 11:09:53 +1030

    use older model

--- jmv_ft.sh
+++ jmv_ft.sh
@@ -5,7 +5,7 @@
 OPUS=build/src
 PATH=${PATH}:${OPUS}
 
-model_id=251002
+model_id=250725
 model=${model_id}/checkpoints/checkpoint_epoch_200.pth
 speech=~/Downloads/all_speech.wav
 speech_test=wav/all.wav
