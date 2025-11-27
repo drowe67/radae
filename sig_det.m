4428708aca534e98ed15b22481f4f8ad6cd9d34e
David <david@rowetel.com>
2025-11-28 08:50:54 +1030

    fixed threshold for sig_det, spot checks look good

--- sig_det.m
+++ sig_det.m
@@ -2,15 +2,24 @@
 % RADE V2 signal detection using Rayleigh model
 % Ry is a ntimestep (rows) by Ncp+M (cols) matrix
 
-function [det,var] = sig_det(Ry)
-  beta = 0.95;
-  x = 0; x_2 = 0;
+function [det,sigma_r,Ry_bar,Ts] = sig_det(Ry)
+  alpha = 0.85; beta = 0.95; Pf=1E-4;
   [ntimesteps, cols] = size(Ry);
+  sigma_r = zeros(1,ntimesteps); sigma_r_prev =0;
+  Ts = zeros(1,ntimesteps);
+  det = zeros(1,ntimesteps);
+
+  % smooth Ry
+  Ry_bar=filter(1-alpha,[1, -alpha],Ry);
+
+  Ry_max = max(abs(Ry_bar'));
   for n=1:ntimesteps
-    % update EWMA variance est for current timesteps
-    x = x*beta + mean(abs(Ry(n,:)))*(1-beta);
-    x_2 = x_2*beta + mean(abs(Ry(n,:)).^2)*(1-beta);
-    var(n) = x_2 - x^2;
-  end  
-  det = 0;
+    % update EWMA sigma_r est for current timestep
+    sigma_r(n) = sigma_r_prev*beta + mean(abs(Ry_bar(n,:)))*(1-beta)/sqrt(pi/2);
+    sigma_r_prev = sigma_r(n);
+    % use Rayleigh model to see if we have detected a signal
+    Ts(n)=sigma_r(n)*sqrt(-2*log(Pf));
+    Ts(n) = 0.42;
+    det(n) = Ry_max(n) > Ts(n);
+  end
 endfunction
