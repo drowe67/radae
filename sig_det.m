1a8c3b248567f858f2a61cf07f260a2126ecf6f2
David <david@rowetel.com>
2025-12-01 10:58:32 +1030

    refactor sig detection script, f_off output

--- sig_det.m
+++ sig_det.m
@@ -2,24 +2,17 @@
 % RADE V2 signal detection using Rayleigh model
 % Ry is a ntimestep (rows) by Ncp+M (cols) matrix
 
-function [det,sigma_r,Ry_bar,Ts] = sig_det(Ry)
-  alpha = 0.85; beta = 0.95; Pf=1E-4;
+function [delta_hat,det,f_off,Ry_bar] = sig_det(Ry,Ts=0.42,Fs=8000,M=128)
+  alpha = 0.85;
   [ntimesteps, cols] = size(Ry);
-  sigma_r = zeros(1,ntimesteps); sigma_r_prev =0;
-  Ts = zeros(1,ntimesteps);
   det = zeros(1,ntimesteps);
 
-  % smooth Ry
   Ry_bar=filter(1-alpha,[1, -alpha],Ry);
-
-  Ry_max = max(abs(Ry_bar'));
-  for n=1:ntimesteps
-    % update EWMA sigma_r est for current timestep
-    sigma_r(n) = sigma_r_prev*beta + mean(abs(Ry_bar(n,:)))*(1-beta)/sqrt(pi/2);
-    sigma_r_prev = sigma_r(n);
-    % use Rayleigh model to see if we have detected a signal
-    Ts(n)=sigma_r(n)*sqrt(-2*log(Pf));
-    Ts(n) = 0.42;
-    det(n) = Ry_max(n) > Ts(n);
+  [Ry_max delta_hat] = max(abs(Ry_bar'));
+  det =  Ry_max > Ts;
+  f_off = zeros(1,ntimesteps);
+  for i=1:ntimesteps
+    delta_phi = angle(Ry_bar(i,delta_hat(i)));
+    f_off(i) = -delta_phi*Fs/(2*pi*M);
   end
 endfunction
